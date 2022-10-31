f9b25c614395218bf604fe850247a6fe102bfb74
malekseeva <maya.alekseeva@orioninc.com>
2022-10-31 16:57:18 +0300

    Added tablet version and burger menu

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,40 @@
 import "./css/style.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+const header = document.getElementById("header")!;
 
-`;
+window.onscroll = () => {
+  "use strict";
+  if (header) {
+    if (
+      document.body.scrollTop >= 280 ||
+      document.documentElement.scrollTop >= 280
+    ) {
+      header.classList.add("scroll");
+    } else {
+      header.classList.remove("scroll");
+    }
+  }
+};
+
+const burger = document.getElementById("burger");
+const nav = document.getElementById("nav");
+
+if (burger && nav) {
+  burger.onclick = () => {
+    burger.classList.toggle("burger__active");
+
+    nav.classList.toggle("nav__active");
+  };
+
+  document.querySelectorAll(".menu-list__item a").forEach((item) => {
+    item.addEventListener("click", () => {
+      burger.classList.toggle("burger__active");
+      nav.classList.toggle("nav__active");
+    });
+  });
+}
+
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+
+// `;
